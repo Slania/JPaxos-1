afcca51e8999021fc5458e038eefcdac58d8e83a
Slania <srivaths90@gmail.com>
2014-02-09 15:00:47 -0600

    Sri | Changing int to long

--- src/lsr/paxos/test/directory/ResultsInterpreter.java
+++ src/lsr/paxos/test/directory/ResultsInterpreter.java
@@ -48,7 +48,7 @@ public class ResultsInterpreter {
             preparedStatement.setString(1, requestIDs.getString("request_id"));
             ResultSet logs = preparedStatement.executeQuery();
             String replicaId;
-            Integer clientSendRequest, clientReceiveReply, nioClientProxyExecute, paxosEnqueueRequest, paxosDecide, serviceExecuteStart, serviceExecuteFinish, nioClientProxySent;
+            Long clientSendRequest, clientReceiveReply, nioClientProxyExecute, paxosEnqueueRequest, paxosDecide, serviceExecuteStart, serviceExecuteFinish, nioClientProxySent;
             String clientSendRequest_s, clientReceiveReply_s, nioClientProxyExecute_s, paxosEnqueueRequest_s, paxosDecide_s, serviceExecuteStart_s, serviceExecuteFinish_s, nioClientProxySent_s;
             while (logs.next()) {
                 replicaId = logs.getString("replica_id");
@@ -57,51 +57,51 @@ public class ResultsInterpreter {
                 }
                 clientSendRequest_s = logs.getString("client_send_request");
                 if (!logs.wasNull()) {
-                    clientSendRequest = Integer.valueOf(clientSendRequest_s);
+                    clientSendRequest = Long.valueOf(clientSendRequest_s);
                 } else {
-                    clientSendRequest = -1;
+                    clientSendRequest = (long) -1;
                 }
                 clientReceiveReply_s = logs.getString("client_receive_reply");
                 if (!logs.wasNull()) {
-                    clientReceiveReply = Integer.valueOf(clientReceiveReply_s);
+                    clientReceiveReply = Long.valueOf(clientReceiveReply_s);
                 } else {
-                    clientReceiveReply = -1;
+                    clientReceiveReply = (long) -1;
                 }
                 nioClientProxyExecute_s = logs.getString("nioclientproxy_execute");
                 if (!logs.wasNull()) {
-                    nioClientProxyExecute = Integer.valueOf(nioClientProxyExecute_s);
+                    nioClientProxyExecute = Long.valueOf(nioClientProxyExecute_s);
                 } else {
-                    nioClientProxyExecute = -1;
+                    nioClientProxyExecute = (long) -1;
                 }
                 paxosEnqueueRequest_s = logs.getString("paxos_enqueuerequest");
                 if (!logs.wasNull()) {
-                    paxosEnqueueRequest = Integer.valueOf(paxosEnqueueRequest_s);
+                    paxosEnqueueRequest = Long.valueOf(paxosEnqueueRequest_s);
                 } else {
-                    paxosEnqueueRequest = -1;
+                    paxosEnqueueRequest = (long) -1;
                 }
-                paxosDecide = Integer.valueOf(logs.getString("paxos_decide"));
+                paxosDecide_s = logs.getString("paxos_decide");
                 if (!logs.wasNull()) {
-                    paxosDecide = -1;
+                    paxosDecide = Long.valueOf(paxosDecide_s);
                 } else {
-                    paxosDecide = -1;
+                    paxosDecide = (long) -1;
                 }
                 serviceExecuteStart_s = logs.getString("service_execute_start");
                 if (!logs.wasNull()) {
-                    serviceExecuteStart = Integer.valueOf(serviceExecuteStart_s);
+                    serviceExecuteStart = Long.valueOf(serviceExecuteStart_s);
                 } else {
-                    serviceExecuteStart = -1;
+                    serviceExecuteStart = (long) -1;
                 }
                 serviceExecuteFinish_s = logs.getString("service_execute_finish");
                 if (logs.wasNull()) {
-                    serviceExecuteFinish = Integer.valueOf(serviceExecuteFinish_s);
+                    serviceExecuteFinish = Long.valueOf(serviceExecuteFinish_s);
                 } else {
-                    serviceExecuteFinish = -1;
+                    serviceExecuteFinish = (long) -1;
                 }
                 nioClientProxySent_s = logs.getString("nioclientproxy_sent");
                 if (!logs.wasNull()) {
-                    nioClientProxySent = Integer.valueOf(nioClientProxySent_s);
+                    nioClientProxySent = Long.valueOf(nioClientProxySent_s);
                 } else {
-                    nioClientProxySent = -1;
+                    nioClientProxySent = (long) -1;
                 }
 
                 System.out.println("Machine: " + replicaId);
