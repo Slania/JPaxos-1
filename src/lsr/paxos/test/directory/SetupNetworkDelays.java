8a923ed7db58474d5b3a286a2e3e9eda58c4c94a
Slania <srivaths90@gmail.com>
2014-03-16 17:44:47 -0600

    Sri | Adding port info to ipfw

--- src/lsr/paxos/test/directory/SetupNetworkDelays.java
+++ src/lsr/paxos/test/directory/SetupNetworkDelays.java
@@ -45,30 +45,50 @@ public class SetupNetworkDelays {
         Integer pipeNumber = 1;
         for (String otherReplica : othersAre) {
             String delay = network.delayBetweenNodes(me, otherReplica);
-            String addPipeCommand = "sudo ipfw add " + ruleNumber + " pipe " + pipeNumber + " ip from " + nsLookUp(getFullName(me)) + " to " + nsLookUp(getFullName(otherReplica));
+            String myPorts = configuration.getProperty(me + ".ports");
+            String otherReplicaPorts = configuration.getProperty(otherReplica + ".ports");
 
-            if(Integer.valueOf(delay) > 0) {
-                String modifyPipeDelayCommand = "sudo ipfw pipe " + pipeNumber + " config delay " + (Integer.valueOf(delay)/2) + "ms";
+            List<String> allMyPorts = new ArrayList<String>();
+            List<String> allOtherReplicaPorts = new ArrayList<String>();
 
-                System.out.println(addPipeCommand);
-                System.out.println(modifyPipeDelayCommand);
+            StringTokenizer portTokenizer = new StringTokenizer(myPorts, ":");
+            while (portTokenizer.hasMoreElements()) {
+                allMyPorts.add((String) portTokenizer.nextElement());
+            }
 
-                builder = new ProcessBuilder("/usr/local/bin/bash", "-c", addPipeCommand);
-                builder.redirectErrorStream(true);
-                process = builder.start();
-                process.waitFor();
+            portTokenizer = new StringTokenizer(otherReplicaPorts, ":");
+            while (portTokenizer.hasMoreElements()) {
+                allOtherReplicaPorts.add((String) portTokenizer.nextElement());
+            }
 
-                builder = new ProcessBuilder("/usr/local/bin/bash", "-c", modifyPipeDelayCommand);
-                builder.redirectErrorStream(true);
-                process = builder.start();
-                process.waitFor();
+            for (String destPort : allOtherReplicaPorts) {
+                for (String srcPort : allMyPorts) {
+                    String addPipeCommand = "sudo ipfw add " + ruleNumber + " pipe " + pipeNumber + " ip from " + nsLookUp(getFullName(me)) + srcPort + " to " + nsLookUp(getFullName(otherReplica) + destPort);
 
-                ruleNumber++;
-                pipeNumber++;
-            } else if (Integer.valueOf(delay) == 0) {
-                System.out.println("0ms delay between nodes: " + me + " and " + otherReplica + ". No ipfw rules will be set.");
-            } else {
-                System.out.println("Delay numbers must be non-negative!");
+                    if(Integer.valueOf(delay) > 0) {
+                        String modifyPipeDelayCommand = "sudo ipfw pipe " + pipeNumber + " config delay " + (Integer.valueOf(delay)/2) + "ms";
+
+                        System.out.println(addPipeCommand);
+                        System.out.println(modifyPipeDelayCommand);
+
+                        builder = new ProcessBuilder("/usr/local/bin/bash", "-c", addPipeCommand);
+                        builder.redirectErrorStream(true);
+                        process = builder.start();
+                        process.waitFor();
+
+                        builder = new ProcessBuilder("/usr/local/bin/bash", "-c", modifyPipeDelayCommand);
+                        builder.redirectErrorStream(true);
+                        process = builder.start();
+                        process.waitFor();
+
+                        ruleNumber++;
+                        pipeNumber++;
+                    } else if (Integer.valueOf(delay) == 0) {
+                        System.out.println("0ms delay between nodes: " + me + " and " + otherReplica + ". No ipfw rules will be set.");
+                    } else {
+                        System.out.println("Delay numbers must be non-negative!");
+                    }
+                }
             }
         }
     }
