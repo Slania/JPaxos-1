241196fece743d17b7fb84343a7aa79d509bcfd6
Slania <srivaths90@gmail.com>
2014-03-16 18:46:07 -0600

    Sri | Trying to fix ports for ipfw

--- src/lsr/paxos/test/directory/SetupNetworkDelays.java
+++ src/lsr/paxos/test/directory/SetupNetworkDelays.java
@@ -63,7 +63,8 @@ public class SetupNetworkDelays {
 
             for (String destPort : allOtherReplicaPorts) {
                 for (String srcPort : allMyPorts) {
-                    String addPipeCommand = "sudo ipfw add " + ruleNumber + " pipe " + pipeNumber + " ip from " + nsLookUp(getFullName(me)) + " " + srcPort + " to " + nsLookUp(getFullName(otherReplica)) + " " + destPort;
+                    String addPipeCommand = "sudo ipfw add " + ruleNumber + " pipe " + pipeNumber + " ip from " + nsLookUp(getFullName(me)) + " to " + nsLookUp(getFullName(otherReplica)) + " src-port " + srcPort + " dst-port " + destPort;
+                    String addPipeCommandForFrags = "sudo ipfw add " + ruleNumber + " pipe " + pipeNumber + " ip from " + nsLookUp(getFullName(me)) + " to " + nsLookUp(getFullName(otherReplica)) + " frag";
 
                     if(Integer.valueOf(delay) > 0) {
                         String modifyPipeDelayCommand = "sudo ipfw pipe " + pipeNumber + " config delay " + (Integer.valueOf(delay)/2) + "ms";
@@ -76,6 +77,13 @@ public class SetupNetworkDelays {
                         process = builder.start();
                         process.waitFor();
 
+                        ruleNumber++;
+
+                        builder = new ProcessBuilder("/usr/local/bin/bash", "-c", addPipeCommandForFrags);
+                        builder.redirectErrorStream(true);
+                        process = builder.start();
+                        process.waitFor();
+
                         builder = new ProcessBuilder("/usr/local/bin/bash", "-c", modifyPipeDelayCommand);
                         builder.redirectErrorStream(true);
                         process = builder.start();
@@ -83,6 +91,7 @@ public class SetupNetworkDelays {
 
                         ruleNumber++;
                         pipeNumber++;
+
                     } else if (Integer.valueOf(delay) == 0) {
                         System.out.println("0ms delay between nodes: " + me + " and " + otherReplica + ". No ipfw rules will be set.");
                     } else {
